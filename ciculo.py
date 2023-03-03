aa8842d8a933d6280d7c4445eea27ef9a526301d
Iamfrans <1323544@senati.pe>
2023-03-03 09:55:16 -0500

    el espacio :v

--- ciculo.py
+++ ciculo.py
@@ -1,6 +1,6 @@
 #math para usar mamaticas en python
 import math
-r = float(input("ingrese el radio"))
+r = float(input("ingrese el radio: "))
 #math.pi para dar el valor
 area = math.pi * (r * r)
 print("El area del circulo es : ", area)
